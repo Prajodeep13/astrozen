a8174f5c97b8fb2ce743768d05ea6ee89c830c43
Prajodeep13 <prajodeep13@gmail.com>
2025-10-06 14:34:44 +0530

    final commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -25,73 +25,43 @@ export const SITE_CONFIG: SiteConfig = {
 
 export const SITE_CONTENT: SiteContent = {
   hero: {
-    name: "Alejandro Múnez",
-    specialty: "Mobile & Web Developer",
+    name: "Prajodeep Patil",
+    specialty: "Web Developer",
     summary:
-      "Developer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
-    email: "example@email.com",
+      "Developer with knowledge of react, next.js. Served as an intern at UK based company. Passionate developer with the indepth knowledge of core computer technology",
+    email: "prajodeep21@gmail.com",
   },
   experience: [
     {
-      company: "Zalmart",
-      position: "Lead Android Developer",
-      startDate: "May 2018",
-      endDate: "Sept 2020",
+      company: "The Pro Educators",
+      position: "Developer",
+      startDate: "June 2025",
+      endDate: "Sept 2025",
       summary: [
-        "Implemented advanced memory management and code optimization techniques, resulting in a reduction in application load time by 40% and a decrease in crashes by 25%. This significantly improved user experience and increased user retention by 20%.",
-        "I led a team of developers in building and integrating new features using Jetpack Android components such as LiveData and ViewModel. This enabled us to build scalable and maintainable applications, reducing the crash rate by 20% and speeding up the time to delivery of new features by 15%.",
-        "Integrated Google Pay for in-app purchases, resulting in a 35% increase in mobile transaction revenue. Additionally, implemented Firebase Analytics to gain insights into user behavior, enabling data-driven optimizations and a 30% increase in user retention.",
+        "I designed a the website for hospital management",
+        "I worked upon an application that is made for the ones who are wishing to study abroad",
+        "Part of Integration and collaboration team",
       ],
     },
-    {
-      company: "Bankit",
-      position: "Mobile Developer",
-      startDate: "Feb 2017",
-      endDate: "May 2018",
-      summary: [
-        "I designed and developed a mobile application using Flutter, allowing it to be deployed on both Android and iOS with a single codebase. This reduced development time by 50% and maintenance costs by 30%, facilitating a consistent user experience on both platforms.",
-        "I integrated biometric authentication and data encryption, significantly improving the security of user data. This implementation resulted in a 40% increase in user trust and a 25% reduction in unauthorized access attempts.",
-      ],
-    },
-    {
-      company: "Driveer",
-      position: "Frontend Developer",
-      startDate: "Jun 2015",
-      endDate: "Oct 2016",
-      summary:
-        "Developed and integrated a real-time vehicle tracking system using WebSockets, improving accuracy and data update in the application. This functionality increased user satisfaction by 30% and reduced customer service inquiries by 25%.",
-    },
+    
   ],
   projects: [
     {
-      name: "Spotifu Music",
-      summary: "A music streaming app that emulates Spotify's core features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
+      name: "QuickHire : A job hiring website",
+      summary: "A job hiring website with a different admin and user panel",
+      linkPreview: "https://github.com/Prajodeep13/Quickhire_job",
+      linkSource: "https://github.com/Prajodeep13/Quickhire_job",
       image: "/spotifu.png",
     },
-    {
-      name: "Shopp App",
-      summary: "An e-commerce platform that replicates Shopify's key features.",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/shopify-clon.png",
-    },
-    {
-      name: "ClonTagram",
-      summary: "A social network that replicates the features of Instagram",
-      linkPreview: "/",
-      linkSource: "https://github.com/immois/astro-zen",
-      image: "/clone-ig.png",
-    },
+  
   ],
   about: {
     description: `
-      Hi, I’m Alejandro Múnez, a passionate Mobile and Web Developer with a knack for crafting seamless digital experiences. With a strong background in both Android and iOS development, as well as front-end web technologies, I thrive in the intersection where creativity meets technology.
+      Hi, I’m Prajodeep Patil, a passionate Web Developer with a knack for crafting seamless digital experiences. With a strong background in front-end and back-end web technologies, I thrive at the intersection where creativity meets technology.
 
-      Over the years, I’ve honed my skills in building robust, user-friendly applications that not only meet the needs of users but also push the boundaries of what’s possible. My projects range from innovative mobile applications to responsive web designs, all with a focus on performance, security, and scalability.
+Over the years, I’ve honed my skills in building robust, user-friendly, and visually appealing web applications, ensuring performance, security, and scalability at every stage. My projects range from interactive user interfaces to fully functional full-stack web solutions, all driven by my commitment to delivering excellence and exceeding user expectations. I take pride in being an excellent web developer who continuously pushes the boundaries of what’s possible in the digital world.
     `,
-    image: "/alejandro-big.jpg",
+    image: "",
   },
 };
 
